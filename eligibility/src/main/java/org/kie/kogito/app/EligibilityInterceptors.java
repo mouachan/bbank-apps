c659acfd00ab40c34f90d212b3c095e4513fe653
mouachan <mourad.ouachani@gmail.com>
2020-09-16 13:30:47 +0200

    openshift yml files deployment

---
+++ eligibility/src/main/java/org/kie/kogito/app/EligibilityInterceptors.java
@@ -0,0 +1,15 @@
+package org.kie.kogito.app;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.ext.Provider;
+ 
+import org.kie.kogito.monitoring.system.interceptor.MetricsInterceptor;
+ 
+@Provider
+public class EligibilityInterceptors extends MetricsInterceptor {
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) {
+        super.filter(requestContext, responseContext);
+    }
+}
