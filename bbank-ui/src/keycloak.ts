4bc41338686693459d3273d7b6e073d1ef636cd9
mouachan <mourad.ouachani@gmail.com>
2020-10-05 11:34:10 +0200

    keycloak openshift url

--- bbank-ui/src/keycloak.ts
+++ bbank-ui/src/keycloak.ts
@@ -3,7 +3,7 @@ import Keycloak from 'keycloak-js';
 // Setup Keycloak instance as needed
 // Pass initialization options as required or leave blank to load from 'keycloak.json'
 const keycloak = Keycloak({
-  url: 'http://localhost:8080/auth',
+  url: 'https://keycloak-bbank-apps.apps.ocp4.ouachani.org/auth',
   realm: 'bbank-apps',
   clientId: 'bbank-ui',
 });
