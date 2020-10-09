52263021b3519387c43ceed39fd5ec2f04f612ce
mouachan <mourad.ouachani@gmail.com>
2020-10-09 11:23:35 +0200

    new idp

--- bbank-ui/src/keycloak.ts
+++ bbank-ui/src/keycloak.ts
@@ -3,7 +3,7 @@ import Keycloak from 'keycloak-js';
 // Setup Keycloak instance as needed
 // Pass initialization options as required or leave blank to load from 'keycloak.json'
 const keycloak = Keycloak({
-  url: 'https://keycloak-bbank-apps.apps.ocp4.ouachani.org/auth',
+  url: 'https://keycloak-idp.apps.ocp4.ouachani.org/auth',
   realm: 'bbank-apps',
   clientId: 'bbank-sso-client',
 });
