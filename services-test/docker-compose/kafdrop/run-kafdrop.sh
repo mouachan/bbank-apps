9dcdd43acc58c88b8ae12bc5b8aff7692baca560
mouachan <mourad.ouachani@gmail.com>
2021-01-11 09:26:27 +0100

    cleaning test files

--- services-test/docker-compose/kafdrop/run-kafdrop.sh
+++
@@ -1,3 +0,0 @@
-java --add-opens=java.base/sun.nio.ch=ALL-UNNAMED \
--jar kafdrop-3.27.0.jar \
---kafka.brokerConnect=localhost:9092
--- services-test/docker-compose/task-console/get-task-console.sh
+++
@@ -1,3 +0,0 @@
-export TASK_CONSOLE_VERSION="0.17.0"
-TASK_CONSOLE_RUNNER=https://search.maven.org/remotecontent?filepath=org/kie/kogito/task-console/${TASK_CONSOLE_VERSION}/task-console-${TASK_CONSOLE_VERSION}-runner.jar
-wget -nc -O task-console-${TASK_CONSOLE_VERSION}-runner.jar ${TASK_CONSOLE_RUNNER}
