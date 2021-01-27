c317a905a388e915ad619ad1c79b9aa80364edde
mouachan <mourad.ouachani@gmail.com>
2021-01-27 09:42:07 +0100

    docker-compose  (mgmt-console, data-index) and readme updated

--- docker-compose/mgmt-services/run-mgmt-console.sh
+++
@@ -1,4 +0,0 @@
-export MGMT_CONSOLE_VERSION="1.0.0.Final"
-MGMT_CONSOLE_RUNNER="https://repository.jboss.org/nexus/service/local/artifact/maven/content?r=public&g=org.kie.kogito&a=management-console&v=${MGMT_CONSOLE_VERSION}&c=runner"
-wget -nc -O management-console-${MGMT_CONSOLE_VERSION}-runner.jar ${MGMT_CONSOLE_RUNNER}
-java -Dquarkus.http.port=8780 -jar management-console-${MGMT_CONSOLE_VERSION}-runner.jar
--- docker-compose/task-console/run-task-console.sh
+++
@@ -1,2 +0,0 @@
-export VERSION=0.16.0
-java -Dquarkus.http.port=8880 -jar task-console-${VERSION}-runner.jar
